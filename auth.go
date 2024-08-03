ff1927ea20a34ea3962327b05e596f555517a85a
tyommik <2fasti.miem@gmail.com>
2024-08-03 13:35:03 +0300

    Update auth.go

--- auth.go
+++ auth.go
@@ -17,7 +17,29 @@ import (
 	"golang.org/x/crypto/bcrypt"
 )
 
-const AUTH_REQUIRED_MSG = "Proxy authentication required.\n"
+const AUTH_REQUIRED_MSG = `<!DOCTYPE html>
+<html>
+<head>
+<title>Welcome to nginx!</title>
+<style>
+    body {
+        width: 35em;
+        margin: 0 auto;
+        font-family: Tahoma, Verdana, Arial, sans-serif;
+    }
+</style>
+</head>
+<body>
+<h1>Welcome to nginx!</h1>
+<p>If you see this page, the nginx web server is successfully installed and
+working. Further configuration is required.</p>
+<p>For online documentation and support please refer to
+<a href="http://nginx.org/">nginx.org</a>.<br/>
+Commercial support is available at
+<a href="http://nginx.com/">nginx.com</a>.</p>
+<p><em>Thank you for using nginx.</em></p>
+</body>
+</html>`
 const BAD_REQ_MSG = "Bad Request\n"
 const AUTH_TRIGGERED_MSG = "Browser auth triggered!\n"
 const EPOCH_EXPIRE = "Thu, 01 Jan 1970 00:00:01 GMT"
